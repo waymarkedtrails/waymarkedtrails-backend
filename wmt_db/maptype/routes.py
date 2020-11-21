727a1f723d176c475549f78a59355d33d94395f1
Sarah Hoffmann <lonvia@denofr.de>
2020-11-21 15:51:34 +0100

    clean up maptypes code

--- wmt_db/maptype/routes.py
+++ wmt_db/maptype/routes.py
@@ -5,7 +5,7 @@
 
 """ Database for the classic route view (hiking, cycling, etc.)
 """
-from collections import namedtuple, OrderedDict
+from sqlalchemy import MetaData, select, text
 
 import osgende
 from osgende.generic import FilteredTable
@@ -14,8 +14,6 @@ from osgende.relations import RelationHierarchy
 from osgende.common.tags import TagStore
 from wmt_shields import ShieldFactory
 
-from sqlalchemy import MetaData, select, text
-
 from ..tables.countries import CountryGrid
 from ..tables.routes import Routes
 from ..tables.guideposts import GuidePosts
@@ -23,62 +21,109 @@ from ..tables.networknodes import NetworkNodes
 from ..tables.updates import UpdatedGeometriesTable
 from ..tables.styles import StyleTable
 
-def create_mapdb(site_config, options, route_class=Routes):
-    setattr(options, 'schema', site_config.DB_SCHEMA)
-    db = osgende.MapDB(options)
-    setattr(db, 'site_config', site_config)
+class RouteMapDB(osgende.MapDB):
+    """ MapDB for standard route-relation-based activities. Adds special
+        views and the ability to make shields.
+    """
 
+    def __init__(self, config, site_config):
+        super().__init__(config)
+        self.site_config = site_config
+
+    def dataview(self):
+        schema = self.get_option('schema', '')
+        if schema:
+            schema += '.'
+
+        sql = f"SELECT geom FROM {self.tables.style.data.key}"
+        if self.site_config.GUIDEPOSTS is not None:
+            sql += f" UNION SELECT geom FROM {self.tables.guideposts.data.key}"
+
+        with self.engine.begin() as conn:
+            conn.execute(f"CREATE OR REPLACE VIEW {schema}data_view AS {sql}")
+
+    def mkshield(self):
+        route = self.tables.routes
+        rel = self.osmdata.relation.data
+        sel = select([rel.c.tags, route.data.c.country, route.data.c.level])\
+                .where(rel.c.id == route.data.c.id)
+
+        donesyms = set()
+
+        with self.engine.begin() as conn:
+            for r in conn.execution_options(stream_results=True).execute(sel):
+                sym = route.symbols.create(TagStore(r["tags"]), r["country"],
+                                           r["level"])
+
+                if sym is not None:
+                    symid = sym.get_id()
+
+                    if symid not in donesyms:
+                        donesyms.add(symid)
+                        route.symbols.write(sym, True)
+
+
+def create_mapdb(site_config, options):
+    setattr(options, 'schema', site_config.DB_SCHEMA)
+    db = RouteMapDB(options, site_config)
+
+    setup_tables(db)
+
+    return db
+
+
+def setup_tables(db, route_class=Routes):
     if not db.get_option('no_engine'):
-        country = CountryGrid(MetaData(), site_config.DB_TABLES.country)
+        country = CountryGrid(MetaData(), db.site_config.DB_TABLES.country)
         if not country.data.exists(db.engine):
             raise RuntimeError("No country table found.")
 
-    db.set_metadata('srid', site_config.DB_SRID)
+    db.set_metadata('srid', db.site_config.DB_SRID)
     db.set_metadata('num_threads', db.get_option('numthreads'))
 
-    tabname = site_config.DB_TABLES
+    tabname = db.site_config.DB_TABLES
 
     # first the update table: stores all modified routes, points
     uptable = db.add_table('updates',
-                  UpdatedGeometriesTable(db.metadata, tabname.change))
+                           UpdatedGeometriesTable(db.metadata, tabname.change))
 
     # First we filter all route relations into an extra table.
     rfilt = db.add_table('relfilter',
-                FilteredTable(db.metadata, tabname.route_filter,
-                              db.osmdata.relation,
-                              text(f"({site_config.DB_ROUTE_SUBSET})")))
+                         FilteredTable(db.metadata, tabname.route_filter,
+                                       db.osmdata.relation,
+                                       text(f"({db.site_config.DB_ROUTE_SUBSET})")))
 
     # Then we create the connection between ways and relations.
     # This also adds geometries.
     relway = db.add_table('relway',
-                 RelationWayTable(db.metadata, tabname.way_relation,
-                                  db.osmdata.way, rfilt, osmdata=db.osmdata))
+                          RelationWayTable(db.metadata, tabname.way_relation,
+                                           db.osmdata.way, rfilt, osmdata=db.osmdata))
 
     # From that create the segmented table.
     segments = db.add_table('segments',
-                   SegmentsTable(db.metadata, tabname.segment, relway,
-                                 (relway.c.rels,)))
+                            SegmentsTable(db.metadata, tabname.segment, relway,
+                                          (relway.c.rels,)))
 
     # hierarchy table for super relations
     rtree = db.add_table('hierarchy',
-                RelationHierarchy(db.metadata, tabname.hierarchy, rfilt))
+                         RelationHierarchy(db.metadata, tabname.hierarchy, rfilt))
 
     # routes table: information about each route
     routes = db.add_table('routes',
-                 route_class(db.metadata, rfilt, relway, rtree,
-                             CountryGrid(MetaData(), tabname.country),
-                             site_config.ROUTES,
-                             ShieldFactory(site_config.ROUTES.symbols,
-                                           site_config.SYMBOLS)))
+                          route_class(db.metadata, rfilt, relway, rtree,
+                                      CountryGrid(MetaData(), tabname.country),
+                                      db.site_config.ROUTES,
+                                      ShieldFactory(db.site_config.ROUTES.symbols,
+                                                    db.site_config.SYMBOLS)))
 
     # finally the style table for rendering
-    style = db.add_table('style',
-                StyleTable(db.metadata, routes, segments, rtree,
-                           site_config.DEFSTYLE, uptable))
+    db.add_table('style',
+                 StyleTable(db.metadata, routes, segments, rtree,
+                            db.site_config.DEFSTYLE, uptable))
 
     # optional table for guide posts
-    if site_config.GUIDEPOSTS is not None:
-        cfg = site_config.GUIDEPOSTS
+    if db.site_config.GUIDEPOSTS is not None:
+        cfg = db.site_config.GUIDEPOSTS
         filt = db.add_table('gp_filter',
                    FilteredTable(db.metadata, cfg.table_name + '_view',
                                  db.osmdata.node, text(cfg.node_subset),
@@ -87,50 +132,11 @@ def create_mapdb(site_config, options, route_class=Routes):
            .set_update_table(uptable)
 
     # optional table for network nodes
-    if site_config.NETWORKNODES is not None:
-        cfg = site_config.NETWORKNODES
+    if db.site_config.NETWORKNODES is not None:
+        cfg = db.site_config.NETWORKNODES
         filt = db.add_table('nnodes_filter',
                    FilteredTable(db.metadata, cfg.table_name + '_view',
                                  db.osmdata.node,
                                  db.osmdata.node.c.tags.has_key(cfg.node_tag),
                                  view_only=True))
         db.add_table('networknodes', NetworkNodes(db.metadata, filt, cfg))
-
-    db.add_function('dataview', _mapdb_dataview)
-    db.add_function('mkshield', _mapdb_mkshield)
-
-    return db
-
-def _mapdb_dataview(db):
-    schema = db.get_option('schema', '')
-    if schema:
-        schema += '.'
-
-    sql = f"SELECT geom FROM {db.tables.style.data.key}"
-    if db.site_config.GUIDEPOSTS is not None:
-        sql += f" UNION SELECT geom FROM {db.tables.guideposts.data.key}"
-
-    with db.engine.begin() as conn:
-        conn.execute(f"CREATE OR REPLACE VIEW {schema}data_view AS {sql}")
-
-def _mapdb_mkshield(db):
-    route = db.tables.routes
-    rel = db.osmdata.relation.data
-    sel = select([rel.c.tags, route.data.c.country, route.data.c.level])\
-            .where(rel.c.id == route.data.c.id)
-
-    donesyms = set()
-
-    with db.engine.begin() as conn:
-        for r in conn.execution_options(stream_results=True).execute(sel):
-            sym = route.symbols.create(TagStore(r["tags"]), r["country"],
-                                       r["level"])
-
-            if sym is not None:
-                symid = sym.get_id()
-
-                if symid not in donesyms:
-                    donesyms.add(symid)
-                    route.symbols.write(sym, True)
-
-
--- wmt_db/maptype/slopes.py
+++ wmt_db/maptype/slopes.py
@@ -5,77 +5,86 @@
 #               2015-2020 Sarah Hoffmann
 """ Database for the combinded route/way view (slopes)
 """
-from collections import namedtuple, OrderedDict
+from sqlalchemy import text, select, and_, column
 
 import osgende
 from osgende.generic import FilteredTable
 from osgende.common.tags import TagStore
 from osgende.lines import GroupedWayTable
-from wmt_shields import ShieldFactory
-
-from sqlalchemy import text, select, func, and_, column, exists, not_
 
 from ..tables.piste import PisteRoutes, PisteWayInfo
-from ..maptype.routes import create_mapdb as create_route_mapdb
+from ..maptype.routes import setup_tables
 
-def create_mapdb(site_config, options):
-    # all the route stuff we take from the RoutesDB implmentation
-    db = create_route_mapdb(site_config, options, PisteRoutes)
+class SlopesMapDB(osgende.MapDB):
+    """ MapDB for activities that may be mapped as relations or simple ways.
+    """
 
-    # now create the additional joined ways
-    tabname = site_config.DB_TABLES
-    subset = and_(text(site_config.DB_WAY_SUBSET),
-                  column('id').notin_(select([db.tables.relway.c.id])))
-    filt = db.add_table('norelway_filter',
-               FilteredTable(db.metadata, tabname.way_table + '_view',
-                             db.osmdata.way, subset))
+    def __init__(self, config, site_config):
+        super().__init__(config)
+        self.site_config = site_config
 
-    ways = db.add_table('ways',
-               PisteWayInfo(db.metadata, tabname.way_table,
-                            filt, db.osmdata, db.tables.updates,
-                            site_config.ROUTES,
-                            db.tables.routes.shield_fab))
+    def dataview(self):
+        schema = self.get_option('schema', '')
+        if schema:
+            schema += '.'
 
-    cols = ('name', 'symbol', 'difficulty', 'piste')
-    db.add_table('joinded_ways',
-                 GroupedWayTable(db.metadata, tabname.joinedway, ways, cols))
+        with self.engine.begin() as conn:
+            conn.execute(f"""CREATE OR REPLACE VIEW {schema}data_view AS
+                            (SELECT geom FROM {self.tables.style.data.key}
+                             UNION SELECT geom FROM {self.tables.ways.data.key})""")
 
-    db.add_function('dataview', _mapdb_dataview)
-    db.add_function('mkshield', _mapdb_mkshield)
+    def mkshield(self):
+        route = self.tables.routes
+        sway = self.tables.ways
+        rel = self.osmdata.relation.data
+        way = self.osmdata.way.data
 
-    return db
+        donesyms = set()
+        self._write_shields(route,
+                            select([rel.c.tags]).where(rel.c.id == route.data.c.id),
+                            donesyms)
+        self._write_shields(sway,
+                            select([way.c.tags]).where(way.c.id == sway.data.c.id),
+                            donesyms)
 
-def _mapdb_dataview(db):
-    schema = db.get_option('schema', '')
-    if schema:
-        schema += '.'
-
-    with db.engine.begin() as conn:
-        conn.execute(f"""CREATE OR REPLACE VIEW {schema}data_view AS
-                        (SELECT geom FROM {db.tables.style.data.key}
-                         UNION SELECT geom FROM {db.tables.ways.data.key})""")
-
-def _mapdb_mkshield(db):
-    route = db.tables.routes
-    sway = db.tables.ways
-
-    rel = db.osmdata.relation.data
-    way = db.osmdata.way.data
-    todo = ((route, select([rel.c.tags]).where(rel.c.id == route.data.c.id)),
-            (sway, select([way.c.tags]).where(way.c.id == sway.data.c.id)))
-
-    donesyms = set()
-
-    with db.engine.begin() as conn:
-        for src, sel in todo:
-            for r in conn.execution_options(stream_results=True).execute(sel):
+    def _write_shields(self, source, subset, donesyms):
+        with self.engine.begin() as conn:
+            for r in conn.execution_options(stream_results=True).execute(subset):
                 tags = TagStore(r["tags"])
-                t, difficulty = db.tables.routes.basic_tag_transform(0, tags)
-                sym = src.symbols.create(tags, '', difficulty)
+                _, difficulty = self.tables.routes.basic_tag_transform(0, tags)
+                sym = source.symbols.create(tags, '', difficulty)
 
                 if sym is not None:
                     symid = sym.get_id()
 
                     if symid not in donesyms:
                         donesyms.add(symid)
-                        src.symbols.write(sym, True)
+                        source.symbols.write(sym, True)
+
+
+def create_mapdb(site_config, options):
+    # all the route stuff we take from the RoutesDB implmentation
+    setattr(options, 'schema', site_config.DB_SCHEMA)
+    db = SlopesMapDB(options, site_config)
+
+    setup_tables(db, PisteRoutes)
+
+    # now create the additional joined ways
+    tabname = site_config.DB_TABLES
+    subset = and_(text(site_config.DB_WAY_SUBSET),
+                  column('id').notin_(select([db.tables.relway.c.id])))
+    filt = db.add_table('norelway_filter',
+                        FilteredTable(db.metadata, tabname.way_table + '_view',
+                                      db.osmdata.way, subset))
+
+    ways = db.add_table('ways',
+                        PisteWayInfo(db.metadata, tabname.way_table,
+                                     filt, db.osmdata, db.tables.updates,
+                                     site_config.ROUTES,
+                                     db.tables.routes.shield_fab))
+
+    cols = ('name', 'symbol', 'difficulty', 'piste')
+    db.add_table('joinded_ways',
+                 GroupedWayTable(db.metadata, tabname.joinedway, ways, cols))
+
+    return db
